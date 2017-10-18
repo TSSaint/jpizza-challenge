3241efd1f63e8472557a567860f4c982d5850478
Thomas Santos <thomas@socialeffort.co>
2017-10-18 00:15:55 -0400

    add location logic

--- solution.js
+++ solution.js
@@ -4,3 +4,24 @@
 // x values are > and <
 // y values are ^ and v
 const visitedHomes = {};
+
+function Movement(x, y, dir) {
+  // location is visited
+  // sets the next home
+  var xPlane = x;
+  var yPlane = y;
+  switch (dir) {
+    case(">"):
+      xPlane = x + 1;
+      break;
+    case("^"):
+      xPlane = x - 1;
+      break;
+    case("^"):
+      yPlane = y + 1;
+      break;
+    case("v"):
+      yPlane = y - 1;
+      break;
+  }
+}
